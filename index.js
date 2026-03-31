8bfed997b9efa85354b562862ee4e0c64f0010df
harshrai9893 <harshofficial0509@gmail.com>
2026-03-31 23:06:10 +0530

    fixing some errors

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use( (req, res,next) => {
 // error handler
 app.use((err,req,res,next)=>{
     let{statusCode =500, message = "something went wrong!" }=err;
-    res.status(statusCode).render("error.ejs",{message}); 
+   return res.status(statusCode).render("error.ejs",{message}); 
     // res.status(statusCode).send(message);
     
 });
