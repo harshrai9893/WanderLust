ea50b9d771a1fdd466fd41690b37e400fccc46c9
harshrai9893 <harshofficial0509@gmail.com>
2026-04-10 11:35:35 +0530

    mongoose error resolve

--- init/index.js
+++ init/index.js
@@ -1,14 +1,24 @@
+require("dotenv").config({ path: "../.env" });
 
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+const dbUrl = process.env.MONGO_URL;
 
-const initDB = async ()=>{
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj)=>({...obj, owner:"69c10149ef867c953852b1ea"}));
-    await Listing.insertMany(initData.data); 
-    console.log("data was initialized");
+async function main() {
+  await mongoose.connect(dbUrl);
+  console.log(" MongoDB Connected");
+}
+
+const initDB = async () => {
+  console.log(" Running initDB...");
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log(" data was initialized");
 };
 
- initDB();
+main()
+  .then(() => initDB())
+  .catch((err) => console.log(err));
+
