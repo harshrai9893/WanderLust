cf2827d3643adcb16c840d9e55d10429c0fc7508
harshrai9893 <harshofficial0509@gmail.com>
2026-04-10 12:14:19 +0530

    some changes in nit/index.js

--- init/index.js
+++ init/index.js
@@ -1,10 +1,11 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const dbUrl = process.env.MONGO_URL;
+console.log("MONGO URL:", process.env.MONGO_URL);
 
 async function main() {
   await mongoose.connect(dbUrl);
